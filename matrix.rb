e845e1bc9cb313e2350cb30ba112a5a9dc80c103
Sergio Miguel <alu0100706468@ull.edu.es>
2013-10-07 12:58:01 +0100

    Primera parte del programa.v2

--- matrix.rb
+++ matrix.rb
@@ -1,1 +1,35 @@
-matrix
+#! /usr/bin/env ruby
+require 'matrix'
+
+control = 1
+while control == 1
+	puts "Introduzca las dimensiones de la matriz A:"
+	puts "Columnas de A: " 
+	aColumnas = gets.to_i
+	puts "Filas de A: " 
+	aFilas = gets.to_i
+	if aColumnas == aFilas
+		matrizA = Matrix.build(aColumnas, aFilas) { 0 + rand(99) }
+		puts "Se ha generado una Matriz aleatoria A"
+		puts matrizA
+		control = 0
+	else
+		puts "La matriz debe ser cuadrada!"
+	end
+end
+control = 1
+while control == 1
+	puts "Introduzca las dimensiones de la matriz B:"
+	puts "Columnas de B: " 
+	bColumnas = gets.to_i
+	puts "Filas de B: " 
+	bFilas = gets.to_i
+	if bColumnas == bFilas
+		matrizB = Matrix.build(bColumnas, bFilas) { 0 + rand(99) }
+		puts "Se ha generado una Matriz aleatoria A"
+		puts matrizB
+		control = 0
+	else
+		puts "La matriz debe ser cuadrada!"
+	end
+end
