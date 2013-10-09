42d0cde05120fa991a7f6e1754acb0013f2fa201
Sergio Miguel <alu0100706468@ull.edu.es>
2013-10-09 12:10:44 +0100

    practica final

--- matrix2.rb
+++ matrix2.rb
@@ -1,4 +1,5 @@
-#! /Users/jonaslopezmesa/.rvm/rubies/ruby-2.0.0-p247/bin/ruby
+#<<<<<<< HEAD
+#! /usr/bin/env ruby
 
 
 def mapmap(a)
@@ -14,6 +15,9 @@ def to_m(a)
 	a = a.map { |r| r.split(/\s+/) }
 	a = mapmap(a) { |x| x.to_f }
 end
+#=======
+
+#>>>>>>> sergio
 
 def read_matrices(fn)
 	text = File.open(fn).read
@@ -25,7 +29,38 @@ def read_matrices(fn)
 	[a, b]
 end
 
+#<<<<<<< HEAD
+def sumar_matrices(a, b)
+	resultado = Array.new(a.length) { Array.new(a[0].length) }
+	if ((a.length == b.length) && (a[0].length == b[0].length))
+		for i in (0...a.length) do
+			for j in (0...a[0].length) do
+				resultado[i][j] = a[i][j] + b[i][j]
+			end
+		end
+		resultado
+	else
+		puts "Las matrices tienen que ser de el mismo múmero de filas y columnas"
+	end
+end
 
+def multiplicar_matrices(a, b)
+	resultado = Array.new(a.length) { Array.new(b[0].length) }
+	if a[0].length == b.length
+		for i in (0...resultado.length) do
+			for j in (0...resultado[0].length) do
+				s = 0
+				for k in (0...a.length) do s += a[i][k] * b[k][j] end
+				resultado[i][j] = s
+			end
+		end
+	else
+		puts "Las matriz A debe tener el mismo número de filas que las columnas de B"
+	end
+	resultado
+end
+principal
+#=======
 def principal
 	a, b = read_matrices('fichero.dat')
 	puts "Se han leído las dos matrices del fichero"
@@ -44,6 +79,4 @@ def principal
 		end
 	end
 end
-
-
-principal
+#>>>>>>> sergio
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Modificamos el mismo fichero (matrix2.rb) en dos ramas distintas (master y sergio). Al hacer un merge, esto generó un conflicto,
+para el cual se llegó a un consenso y se eliminó uno de los shebangs y se dejó el resto de funciones de manera ordenada.
